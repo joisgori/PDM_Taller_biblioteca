71cc4b8dc6f674c4f22865b2e121f2c006614110
joisgori <37356528+joisgori@users.noreply.github.com>
2019-05-24 14:14:03 -0600

    Creado el viewModel
    Creo que faltan un par de métodos

--- app/src/main/java/com/example/tallerbiblioteca/ViewModels/BookViewModel.kt
+++ app/src/main/java/com/example/tallerbiblioteca/ViewModels/BookViewModel.kt
@@ -1,4 +1,42 @@
 package com.example.tallerbiblioteca.ViewModels
 
-class BookViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.room.RoomDatabase
+import com.example.tallerbiblioteca.Database.DAOs.AuthorDAO
+import com.example.tallerbiblioteca.Database.DAOs.BookDAO
+import com.example.tallerbiblioteca.Database.DAOs.TagDAO
+import com.example.tallerbiblioteca.Database.LibraryRoomDatabase
+import com.example.tallerbiblioteca.Models.Book
+import com.example.tallerbiblioteca.Repository.BookRepository
+
+class BookViewModel(app : Application) : AndroidViewModel(app) {
+
+    private val repository : BookRepository
+
+    init {
+
+        val book : BookDAO = LibraryRoomDatabase.getInstance(app).bookDao()
+        val autor : AuthorDAO = LibraryRoomDatabase.getInstance(app).authorDAO()
+        val tag : TagDAO = LibraryRoomDatabase.getInstance(app).tagDAO()
+
+        repository = BookRepository(book, autor, tag)
+
+    }
+
+    fun findBook(isbn: String): Book {
+
+        return repository.findBook(isbn)
+    }
+
+    fun retrieveStarred(): LiveData<List<Book>> {
+
+        return repository.retrieveStarred()
+    }
+
+    fun retrieveAllBooks():LiveData<List<Book>>{
+
+        return repository.retrieveAllBooks()
+    }
 }
